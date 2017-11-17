0660840dc69d965cbb4c2d1015167cfdfc1c84c3
Paul Brodersen <paulbrodersen@gmail.com>
2017-11-17 10:32:08 +0000

    Added comments

--- netgraph.py
+++ netgraph.py
@@ -73,8 +73,10 @@ import matplotlib.cbook as cb
 from matplotlib.colors import colorConverter, Colormap
 from matplotlib.collections import LineCollection
 
-BASE_NODE_SIZE = 1e-2
-BASE_EDGE_WIDTH = 1e-2
+
+BASE_NODE_SIZE = 1e-2 # i.e. node sizes are in percent of "axis space" (x,y <- [0, 1], [0,1])
+BASE_EDGE_WIDTH = 1e-2 # i.e. edge widths are in percent of "axis space" (x,y <- [0, 1], [0,1])
+
 
 def draw(adjacency_matrix, node_positions=None, node_labels=None, edge_labels=None, ax=None, **kwargs):
     """
