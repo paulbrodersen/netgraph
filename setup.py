eb2f3e4fd873d4794e60412b39ad0e4b765253b2
Paul Brodersen <paulbrodersen@gmail.com>
2021-05-20 14:15:02 +0100

    Add long description to setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+import os.path
+
 from setuptools import setup, find_packages
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
 setup(
     name = 'netgraph',
     version = '4.0.0',
     description = 'Python drawing utilities for publication quality plots of networks.',
+    long_description = read_file('README.md'),
+    long_description_content_type = 'text/markdown',
     author = 'Paul Brodersen',
     author_email = 'paulbrodersen+netgraph@gmail.com',
     url = 'https://github.com/paulbrodersen/netgraph',
