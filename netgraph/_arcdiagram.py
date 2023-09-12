beeec430058b120e43ed284f6a6a992f9b88449f
Paul Brodersen <paulbrodersen@gmail.com>
2023-09-12 11:05:06 +0100

    Improve ArcDiagram documentation

--- netgraph/_arcdiagram.py
+++ netgraph/_arcdiagram.py
@@ -53,11 +53,6 @@ class BaseArcDiagram(BaseGraph):
     node_layout : str or dict, default 'linear'
         If `node_layout` is a string, the node positions are computed using the indicated method:
 
-        - 'random'    : place nodes in random positions;
-        - 'circular'  : place nodes regularly spaced on a circle;
-        - 'spring'    : place nodes using a force-directed layout (Fruchterman-Reingold algorithm);
-        - 'dot'       : place nodes using the Sugiyama algorithm; the graph should be directed and acyclic;
-        - 'community' : place nodes such that nodes belonging to the same community are grouped together
         - 'linear'    : place nodes one a horizontal line
 
         If `node_layout` is a dict, keys are nodes and values are (x, y) positions.
@@ -65,11 +60,6 @@ class BaseArcDiagram(BaseGraph):
         Keyword arguments passed to node layout functions.
         See the documentation of the following functions for a full description of available options:
 
-        - get_random_layout
-        - get_circular_layout
-        - get_fruchterman_reingold_layout
-        - get_sugiyama_layout
-        - get_community_layout
         - get_linear_layout
 
     node_shape : str or dict, default 'o'
@@ -268,11 +258,6 @@ class ArcDiagram(BaseArcDiagram, Graph):
     node_layout : str or dict, default 'linear'
         If `node_layout` is a string, the node positions are computed using the indicated method:
 
-        - 'random'    : place nodes in random positions;
-        - 'circular'  : place nodes regularly spaced on a circle;
-        - 'spring'    : place nodes using a force-directed layout (Fruchterman-Reingold algorithm);
-        - 'dot'       : place nodes using the Sugiyama algorithm; the graph should be directed and acyclic;
-        - 'community' : place nodes such that nodes belonging to the same community are grouped together
         - 'linear'    : place nodes one a horizontal line
 
         If `node_layout` is a dict, keys are nodes and values are (x, y) positions.
@@ -280,11 +265,6 @@ class ArcDiagram(BaseArcDiagram, Graph):
         Keyword arguments passed to node layout functions.
         See the documentation of the following functions for a full description of available options:
 
-        - get_random_layout
-        - get_circular_layout
-        - get_fruchterman_reingold_layout
-        - get_sugiyama_layout
-        - get_community_layout
         - get_linear_layout
 
     node_shape : str or dict, default 'o'
@@ -510,11 +490,6 @@ class InteractiveArcDiagram(DraggableArcDiagram, EmphasizeOnHoverGraph, Annotate
     node_layout : str or dict, default 'linear'
         If `node_layout` is a string, the node positions are computed using the indicated method:
 
-        - 'random'    : place nodes in random positions;
-        - 'circular'  : place nodes regularly spaced on a circle;
-        - 'spring'    : place nodes using a force-directed layout (Fruchterman-Reingold algorithm);
-        - 'dot'       : place nodes using the Sugiyama algorithm; the graph should be directed and acyclic;
-        - 'community' : place nodes such that nodes belonging to the same community are grouped together
         - 'linear'    : place nodes one a horizontal line
 
         If `node_layout` is a dict, keys are nodes and values are (x, y) positions.
@@ -791,14 +766,12 @@ class MutableArcDiagram(InteractiveArcDiagram, MutableGraph):
     - Pressing 'delete' or '-' will remove selected nodes and edges.
     - Pressing '@' will reverse the direction of selected edges.
 
-    Notes
-    -----
     When adding a new node, the properties of the last selected node will be used to style the node artist.
     Ditto for edges. If no node or edge has been previously selected the first created node or edge artist will be used.
 
     See also
     --------
-    ArcDiagram, InteractiveArcDiagram
+    ArcDiagram, InteractiveArcDiagram, EditableArcDiagram
 
     """
 
@@ -867,10 +840,241 @@ class EditableArcDiagram(MutableArcDiagram, EditableGraph):
     - To create or edit an annotation, select the node (or edge) artist, press 'alt'+'enter', and type.
     - Terminate either action by pressing 'enter' or 'alt'+'enter' a second time.
 
-    Notes
-    -----
     When adding a new node, the properties of the last selected node will be used to style the node artist.
     Ditto for edges. If no node or edge has been previously selected the first created node or edge artist will be used.
+
+    Parameters
+    ----------
+    graph : various formats
+        Graph object to plot. Various input formats are supported.
+        In order of precedence:
+
+        - Edge list:
+          Iterable of (source, target) or (source, target, weight) tuples,
+          or equivalent (E, 2) or (E, 3) ndarray, where E is the number of edges.
+        - Adjacency matrix:
+          Full-rank (V, V) ndarray, where V is the number of nodes/vertices.
+          The absence of a connection is indicated by a zero.
+
+          .. note:: If V <= 3, any (2, 2) or (3, 3) matrices will be interpreted as edge lists.
+
+        - networkx.Graph, igraph.Graph, or graph_tool.Graph object
+
+    node_order : list or None, default None
+        The ordering of nodes (left-to-right).
+        If None, the node order is optimised such that the number of edge crossings is minimal.
+    above : bool, default True
+        If True, edges arc above the line of nodes.
+        If False, edges arc below.
+    node_layout : str or dict, default 'linear'
+        If `node_layout` is a string, the node positions are computed using the indicated method:
+
+        - 'linear'    : place nodes one a horizontal line
+
+        If `node_layout` is a dict, keys are nodes and values are (x, y) positions.
+    node_layout_kwargs : dict or None, default None
+        Keyword arguments passed to node layout functions.
+        See the documentation of the following functions for a full description of available options:
+
+        - get_random_layout
+        - get_circular_layout
+        - get_fruchterman_reingold_layout
+        - get_sugiyama_layout
+        - get_community_layout
+        - get_linear_layout
+
+    node_shape : str or dict, default 'o'
+        Node shape.
+        If the type is str, all nodes have the same shape.
+        If the type is dict, maps each node to an individual string representing the shape.
+        The string specification is as for matplotlib.scatter marker, i.e. one of 'so^>v<dph8'.
+    node_size : float or dict, default 3.
+        Node size (radius).
+        If the type is float, all nodes will have the same size.
+        If the type is dict, maps each node to an individual size.
+
+        .. note:: Values are rescaled by BASE_SCALE (1e-2) to be compatible with layout routines in igraph and networkx.
+
+    node_edge_width : float or dict, default 0.5
+        Line width of node marker border.
+        If the type is float, all nodes have the same line width.
+        If the type is dict, maps each node to an individual line width.
+
+        ..note:: Values are rescaled by BASE_SCALE (1e-2) to be compatible with layout routines in igraph and networkx.
+
+    node_color : matplotlib color specification or dict, default 'w'
+        Node color.
+        If the type is a string or RGBA array, all nodes have the same color.
+        If the type is dict, maps each node to an individual color.
+    node_edge_color : matplotlib color specification or dict, default DEFAULT_COLOR
+        Node edge color.
+        If the type is a string or RGBA array, all nodes have the same edge color.
+        If the type is dict, maps each node to an individual edge color.
+    node_alpha : scalar or dict, default 1.
+        Node transparency.
+        If the type is a float, all nodes have the same transparency.
+        If the type is dict, maps each node to an individual transparency.
+    node_zorder : int or dict, default 2
+        Order in which to plot the nodes.
+        If the type is an int, all nodes have the same zorder.
+        If the type is dict, maps each node to an individual zorder.
+    node_labels : bool or dict, (default False)
+        If False, the nodes are unlabelled.
+        If True, the nodes are labelled with their node IDs.
+        If the node labels are to be distinct from the node IDs, supply a dictionary mapping nodes to node labels.
+        Only nodes in the dictionary are labelled.
+    node_label_offset: float or tuple, default (0., 0.)
+        A (dx, dy) tuple specifies the exact offset from the node position.
+        If a single scalar delta is specified, the value is interpreted as a distance,
+        and the label is placed delta away from the node position while trying to
+        reduce node/label, node/edge, and label/label overlaps.
+    node_label_fontdict : dict
+        Keyword arguments passed to matplotlib.text.Text.
+        For a full list of available arguments see the matplotlib documentation.
+        The following default values differ from the defaults for matplotlib.text.Text:
+
+        - size (adjusted to fit into node artists if offset is (0, 0))
+        - horizontalalignment (default here: 'center')
+        - verticalalignment (default here: 'center')
+        - clip_on (default here: False)
+        - zorder (default here: inf)
+
+    edge_width : float or dict, default 1.
+        Width of edges.
+        If the type is a float, all edges have the same width.
+        If the type is dict, maps each edge to an individual width.
+
+        .. note:: Value is rescaled by BASE_SCALE (1e-2) to be compatible with layout routines in igraph and networkx.
+
+    edge_cmap : matplotlib color map (default 'RdGy')
+        Color map used to map edge weights to edge colors. Should be diverging.
+        If edge weights are strictly positive, weights are mapped to the
+        left hand side of the color map with vmin=0 and vmax=np.max(weights).
+        If edge weights are positive and negative, then weights are mapped
+        to colors such that a weight of zero corresponds to the center of the
+        color map; the boundaries are set to +/- the maximum absolute weight.
+        If the graph is unweighted or the edge colors are specified explicitly,
+        this parameter is ignored.
+    edge_color : matplotlib color specification or dict, default DEFAULT_COLOR
+        Edge color. If provided explicitly, overrides `edge_cmap`.
+        If the type is a string or RGBA array, all edges have the same color.
+        If the type is dict, maps each edge to an individual color.
+    edge_alpha : float or dict, default 1.
+        The edge transparency,
+        If the type is a float, all edges have the same transparency.
+        If the type is dict, maps each edge to an individual transparency.
+    edge_zorder : int or dict, default 1
+        Order in which to plot the edges.
+        If the type is an int, all nodes have the same zorder.
+        If the type is dict, maps each node to an individual zorder.
+        If None, the edges will be plotted in the order they appear in 'adjacency'.
+        Hint: graphs typically appear more visually pleasing if darker edges are plotted on top of lighter edges.
+    arrows : bool, default False
+        If True, draw edges with arrow heads.
+    edge_layout_kwargs : dict, default None
+        Keyword arguments passed to :code:`get_arced_edge_paths`.
+        Possible keyword arguments are:
+
+        - :code:`rad` : float, default 1.
+          Controls the curvature of the arcs.
+        - :code:`selfloop_radius` : float, default 0.05 * np.linalg.norm(scale)
+          Selfloop radius.
+        - :code:`selfloop_angle` : float, default np.pi / 2
+          Selfloop start angle
+
+    edge_labels : bool or dict, default False
+        If False, the edges are unlabelled.
+        If True, the edges are labelled with their edge IDs.
+        If the edge labels are to be distinct from the edge IDs, supply a dictionary mapping edges to edge labels.
+        Only edges in the dictionary are labelled.
+    edge_label_position : float, default 0.5
+        Relative position along the edge where the label is placed.
+
+        - head   : 0.
+        - centre : 0.5
+        - tail   : 1.
+
+    edge_label_rotate : bool, default True
+        If True, edge labels are rotated such that they have the same orientation as their edge.
+        If False, edge labels are not rotated; the angle of the text is parallel to the axis.
+    edge_label_fontdict : dict
+        Keyword arguments passed to matplotlib.text.Text.
+        For a full list of available arguments see the matplotlib documentation.
+        The following default values differ from the defaults for matplotlib.text.Text:
+
+        - horizontalalignment (default here: 'center'),
+        - verticalalignment (default here: 'center')
+        - clip_on (default here: False),
+        - bbox (default here: dict(boxstyle='round', ec=(1.0, 1.0, 1.0), fc=(1.0, 1.0, 1.0)),
+        - zorder (default here: inf),
+        - rotation (determined by edge_label_rotate argument)
+
+    annotations : dict
+        Mapping of nodes or edges to strings or dictionaries, the annotations.
+        The visibility of the annotations can be toggled on or off by clicking on the corresponding node or edge.
+
+        .. line-block::
+           annotations = {
+               0      : 'Normal node',
+               1      : {s : 'Less important node', fontsize : 2},
+               2      : {s : 'Very important node', fontcolor : 'red'},
+               (0, 1) : 'Normal edge',
+               (1, 2) : {s : 'Less important edge', fontsize : 2},
+               (2, 0) : {s : 'Very important edge', fontcolor : 'red'},
+           }
+
+    annotation_fontdict : dict
+        Keyword arguments passed to matplotlib.text.Text if only the annotation string is given.
+        For a full list of available arguments see the matplotlib documentation.
+        The following default values differ from the defaults for matplotlib.text.Text:
+
+        - horizontalalignment (depends on node position or edge orientation),
+        - verticalalignment (depends on node position or edge orientation),
+        - clip_on (default here: False),
+        - backgroundcolor (default here: 'white'),
+        - zorder (default here: inf),
+
+    tables : dict node/edge : pandas dataframe
+        Mapping of nodes and/or edges to pandas dataframes.
+        The visibility of the tables that can toggled on or off by clicking on the corresponding node or edge.
+    table_kwargs : dict
+        Keyword arguments passed to matplotlib.pyplot.table.
+    origin : tuple, default (0., 0.)
+        The lower left hand corner of the bounding box specifying the extent of the canvas.
+    scale : tuple, default (1., 1.)
+        The width and height of the bounding box specifying the extent of the canvas.
+    prettify : bool, default True
+        If True, despine and remove ticks and tick labels.
+        Set figure background to white. Set axis aspect to equal.
+    ax : matplotlib.axis instance or None, default None
+        Axis to plot onto; if none specified, one will be instantiated with plt.gca().
+
+    Attributes
+    ----------
+    node_artists : dict
+        Mapping of node IDs to matplotlib PathPatch artists.
+    edge_artists : dict
+        Mapping of edge IDs to matplotlib PathPatch artists.
+    node_label_artists : dict
+        Mapping of node IDs to matplotlib text objects (if applicable).
+    edge_label_artists : dict
+        Mapping of edge IDs to matplotlib text objects (if applicable).
+    node_positions : dict node : (x, y) tuple
+        Mapping of node IDs to node positions.
+
+    Notes
+    -----
+    You must retain a reference to the plot instance!
+    Otherwise, the plot instance will be garbage collected after the initial draw
+    and you won't be able to move the plot elements around.
+
+    Examples
+    --------
+    >>> import matplotlib.pyplot as plt
+    >>> from netgraph import InteractiveArcDiagram
+    >>> plt.ion()
+    >>> plot_instance = EditableArcDiagram(my_graph_obj)
+    >>> plt.show()
 
     See also
     --------
