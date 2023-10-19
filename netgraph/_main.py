Bugfix: correctly handle `radians` flag

netgraph/_main.py
@@ -883,7 +883,7 @@ class BaseGraph(object):
 
                 # get tangent in degrees
                 dx, dy = _get_tangent_at_point(edge_artist.midline, edge_label_position)
-                angle = _get_angle(dx, dy, radians=True)
+                angle = _get_angle(dx, dy, radians=False)
 
                 # make label orientation "right-side-up"
                 if angle > 90:
@@ -921,7 +921,7 @@ class BaseGraph(object):
                         self.edge_artists[edge].midline,
                         self.edge_label_position
                     )
-                    angle = _get_angle(dx, dy, radians=True)
+                    angle = _get_angle(dx, dy, radians=False)
                     # make label orientation "right-side-up"
                     if angle > 90:
                         angle -= 180
@@ -2004,7 +2004,7 @@ class AnnotateOnClick(object):
 
     def _get_text_alignment(self, vector):
         dx, dy = vector
-        angle = _get_angle(dx, dy, radians=True) % 360
+        angle = _get_angle(dx, dy, radians=False) % 360
 
         if (45 <= angle < 135):
             horizontalalignment = 'center'
@@ -2167,7 +2167,7 @@ class AnnotateOnClickGraph(Graph, AnnotateOnClick):
         tangent = _get_tangent_at_point(artist.midline, 0.5)
         orthogonal_vector = _get_orthogonal_unit_vector(np.atleast_2d(tangent)).ravel()
         vector_pointing_outwards = self._get_vector_pointing_outwards(midpoint)
-        if _get_interior_angle_between(orthogonal_vector, vector_pointing_outwards, radians=True) > 90:
+        if _get_interior_angle_between(orthogonal_vector, vector_pointing_outwards, radians=False) > 90:
             orthogonal_vector *= -1
 
         x, y = midpoint + 2 * artist.width * orthogonal_vector

netgraph/_utils.py
@@ -195,22 +195,23 @@ def _bspline(cv, n=100, degree=5, periodic=False):
     return spl(np.linspace(0,max_param,n))
 
 
-def _get_angle(dx, dy, radians=False):
+def _get_angle(dx, dy, radians=True):
     """Angle of a vector in 2D."""
     angle = np.arctan2(dy, dx)
     if radians:
-        angle *= 360 / (2.0 * np.pi)
-    return angle
+        return angle
+    else:
+        return angle * 360 / (2.0 * np.pi)
 
 
-def _get_interior_angle_between(v1, v2, radians=False):
+def _get_interior_angle_between(v1, v2, radians=True):
     """Returns the interior angle between vectors v1 and v2.
 
     Parameters
     ----------
     v1, v2 : numpy.array
         The vectors in question.
-    radians : bool, default False
+    radians : bool, default True
         If True, return the angle in radians (otherwise it is in degrees).
 
     Returns
@@ -241,8 +242,9 @@ def _get_interior_angle_between(v1, v2, radians=False):
     v2_u = _get_unit_vector(v2)
     angle = np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
     if radians:
-        angle *= 360 / (2 * np.pi)
-    return angle
+        return angle
+    else:
+        return angle * 360 / (2 * np.pi)
 
 
 def _get_unit_vector(vector):
@@ -250,14 +252,14 @@ def _get_unit_vector(vector):
     return vector / np.linalg.norm(vector)
 
 
-def _get_signed_angle_between(v1, v2, radians=False):
+def _get_signed_angle_between(v1, v2, radians=True):
     """Returns the signed angle between vectors v1 and v2.
 
     Parameters
     ----------
     v1, v2 : numpy.array
         The vectors in question.
-    radians : bool, default False
+    radians : bool, default True
         If True, return the angle in radians (otherwise it is in degrees).
 
     Returns
@@ -281,8 +283,9 @@ def _get_signed_angle_between(v1, v2, radians=False):
     det = x1*y2 - y1*x2
     angle = np.arctan2(det, dot)
     if radians:
-        angle *= 360 / (2 * np.pi)
-    return angle
+        return angle
+    else:
+        return angle * 360 / (2 * np.pi)
 
 
 def _get_n_points_on_a_circle(xy, radius, n, start_angle=0):