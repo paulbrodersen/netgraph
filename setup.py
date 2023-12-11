8733b3088250349df778954e69d28a7675222c3b
Paul Brodersen <paulbrodersen@gmail.com>
2023-12-11 11:46:11 +0000

    Correct release tag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read_file(filename):
     with open(os.path.join(os.path.dirname(__file__), filename)) as file:
         return file.read()
 
-version = '4.13.1'
+version = '4.13.2'
 
 setup(
     name='netgraph',
